38e39ce183229391bb49a939298da6294ae5a688
lukcyn <lukaszcyndzerr@gmail.com>
2023-12-29 10:23:26 +0100

    Added redirect from '/'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import styles from './page.module.css'
-
+"use client";
+import { useRouter } from "next/navigation";
+import { useUserRole } from "@/context/UserRoleContext";
+import { useEffect } from "react";
+import { AppUserRole } from "@/enums/role";
 
 export default function Home() {
-  return (
-    <div className={styles.main}>
-    </div>
-  )
+  const router = useRouter();
+  const { userRole } = useUserRole();
+
+  useEffect(() => {
+    switch (userRole) {
+      case AppUserRole.CANDIDATE:
+        router.push("/home/candidate");
+        break;
+      case AppUserRole.ADMIN:
+        router.push("/home/admin");
+        break;
+      case AppUserRole.ADMINISTRATION_EMPLOYEE:
+        router.push("/home/administration");
+        break;
+      default:
+        router.push("/login");
+        break;
+    }
+  }, [userRole]);
+
+  return <div></div>;
 }
